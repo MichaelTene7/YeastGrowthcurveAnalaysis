98da780a24644ebdc7bd9480788cdc422bfdc5a6
MichaelTene7 <110479175+MichaelTene7@users.noreply.github.com>
2024-02-01 10:50:08 -0500

    Add axis labels and also tidying of old sections

--- src/loc/YeastStrainsCreationAndMedia.R
+++ src/loc/YeastStrainsCreationAndMedia.R
@@ -70,6 +70,8 @@ wellNames = names(cleanData)
 #allWells = names(cleanData)
 #combinedPlot(allWells)
 
+
+{ #collapse this bracket set, it is all for use making older plots 
 wells2490 = wellNames[grep("2490", wellNames)]
 combinedPlot(wells2490)
 wells2490mm = wellNames[grepl("2490", wellNames) & grepl("Mm", wellNames)]
@@ -112,7 +114,7 @@ grid.arrange(wellsASMmPlot, wellsASMm5Plot, ncol = 2)
 
 
 combinedPlot(c("Glu_1_Mm_2490","Glu_5_Mm_2490"))
-
+}
 
 # New plot remake 
 wellsSerMm = wellNames[grepl("Ser_1", wellNames) & grepl("Mm", wellNames)]
@@ -129,7 +131,7 @@ cleanData = cbind(cleanData, serMmDeletionAverage)
 serWellSet = append(wellsSerMm, c("serMmControlAverage", "serMmDeletionAverage"))
 
 wellsSerMmPlotNew = combinedPlotColor(serWellSet, colorsetNumber = 5, groupedNumber = 3, groupRepeatNumber = 2, colorOrder = c("backgroundRed", "backgroundBlue", "backgroundRed", "backgroundBlue", "boldAverages"))
-
+wellsSerMmPlotNew
 
 
 #
@@ -149,78 +151,71 @@ growthcurverOutput = read.csv("Output/yeastStrains.csv")
 growthRates = growthcurverOutput$r
 names(growthRates) = growthcurverOutput$sample
 
-growthcurverOutputPlot = growthcurverOutput
-growthcurverOutputPlot$r[growthcurverOutputPlot$r >1] =NA
-growthcurverOutputPlot$strain = growthcurverOutputPlot$sample
 
-growthcurverOutputPlot$strain[grep("2490", growthcurverOutputPlot$sample)] = "2490"
-growthcurverOutputPlot$strain[grep("2491", growthcurverOutputPlot$sample)] = "2491"
-growthcurverOutputPlot$strain[grep("2492", growthcurverOutputPlot$sample)] = "2492"
-growthcurverOutputPlot$strain[grep("2493", growthcurverOutputPlot$sample)] = "2493"
-growthcurverOutputPlot$media = growthcurverOutputPlot$sample
-growthcurverOutputPlot$media[grep("As", growthcurverOutputPlot$sample)] = "As"
-growthcurverOutputPlot$media[grep("Glu", growthcurverOutputPlot$sample)] = "Glu"
-growthcurverOutputPlot$media[grep("Ser", growthcurverOutputPlot$sample)] = "Ser"
-growthcurverOutputPlot$media[grep("Thr", growthcurverOutputPlot$sample)] = "Thr"
+#Add metadata to the growthcurver Data 
+{
+  growthcurverOutputPlot = growthcurverOutput
+  growthcurverOutputPlot$r[growthcurverOutputPlot$r >1] =NA
+  growthcurverOutputPlot$strain = growthcurverOutputPlot$sample
+  
+  growthcurverOutputPlot$strain[grep("2490", growthcurverOutputPlot$sample)] = "2490"
+  growthcurverOutputPlot$strain[grep("2491", growthcurverOutputPlot$sample)] = "2491"
+  growthcurverOutputPlot$strain[grep("2492", growthcurverOutputPlot$sample)] = "2492"
+  growthcurverOutputPlot$strain[grep("2493", growthcurverOutputPlot$sample)] = "2493"
+  growthcurverOutputPlot$media = growthcurverOutputPlot$sample
+  growthcurverOutputPlot$media[grep("As", growthcurverOutputPlot$sample)] = "As"
+  growthcurverOutputPlot$media[grep("Glu", growthcurverOutputPlot$sample)] = "Glu"
+  growthcurverOutputPlot$media[grep("Ser", growthcurverOutputPlot$sample)] = "Ser"
+  growthcurverOutputPlot$media[grep("Thr", growthcurverOutputPlot$sample)] = "Thr"
+  
+  growthcurverOutputPlot$concentration = growthcurverOutputPlot$sample
+  growthcurverOutputPlot$concentration[grep("_5", growthcurverOutputPlot$sample)] = "5%"
+  growthcurverOutputPlot$concentration[grep("_1", growthcurverOutputPlot$sample)] = "1%"
+  
+  growthcurverOutputPlot$plate = growthcurverOutputPlot$sample
+  growthcurverOutputPlot$plate = substr(growthcurverOutputPlot$plate, nchar(growthcurverOutputPlot$plate), nchar(growthcurverOutputPlot$plate))
+  
+  growthcurverOutputPlot$label = growthcurverOutputPlot$strain
+  growthcurverOutputPlot$label = paste(growthcurverOutputPlot$label, growthcurverOutputPlot$plate, sep="")
+  
+  growthcurverOutputPlot$type = growthcurverOutputPlot$strain
+  growthcurverOutputPlot$type[growthcurverOutputPlot$type == 2490 | growthcurverOutputPlot$type ==  2492] = "Deletion"
+  growthcurverOutputPlot$type[growthcurverOutputPlot$type == 2491 | growthcurverOutputPlot$type ==  2493] = "Control"
+  
+  growthcurverOutputPlot$sorter = growthcurverOutputPlot$media
+  growthcurverOutputPlot$sorter = paste(growthcurverOutputPlot$sorter, growthcurverOutputPlot$type, sep = "-")
+  
+  grep("Mm", growthcurverOutputPlot$sample)
+  growthcurverOutputPlotTrimmed = growthcurverOutputPlot[grep("Mm", growthcurverOutputPlot$sample),]
+  
+  growthcurverOutputPlotTrimmed = growthcurverOutputPlotTrimmed[-grep("Ser_5", growthcurverOutputPlotTrimmed$sample),]
+  growthcurverOutputPlotTrimmed = growthcurverOutputPlotTrimmed[-grep("Thr_5", growthcurverOutputPlotTrimmed$sample),]
+  growthcurverOutputPlotTrimmed = growthcurverOutputPlotTrimmed[-grep("Glu_5", growthcurverOutputPlotTrimmed$sample),]
+  growthcurverOutputPlotTrimmed = growthcurverOutputPlotTrimmed[-grep("As_1", growthcurverOutputPlotTrimmed$sample),]
+  
+  
+  growthcurverOutputPlotSerine = growthcurverOutputPlotTrimmed
+  growthcurverOutputPlotSerine = growthcurverOutputPlotSerine[-grep("As_5", growthcurverOutputPlotSerine$sample),]
+  growthcurverOutputPlotSerine = growthcurverOutputPlotSerine[-grep("Thr_1", growthcurverOutputPlotSerine$sample),]
+  
+  growthcurverOutputPlotThreonine = growthcurverOutputPlotTrimmed
+  growthcurverOutputPlotThreonine = growthcurverOutputPlotThreonine[-grep("As_5", growthcurverOutputPlotThreonine$sample),]
+  growthcurverOutputPlotThreonine = growthcurverOutputPlotThreonine[-grep("Ser_1", growthcurverOutputPlotThreonine$sample),]
+}
 
-growthcurverOutputPlot$concentration = growthcurverOutputPlot$sample
-growthcurverOutputPlot$concentration[grep("_5", growthcurverOutputPlot$sample)] = "5%"
-growthcurverOutputPlot$concentration[grep("_1", growthcurverOutputPlot$sample)] = "1%"
-
-growthcurverOutputPlot$plate = growthcurverOutputPlot$sample
-growthcurverOutputPlot$plate = substr(growthcurverOutputPlot$plate, nchar(growthcurverOutputPlot$plate), nchar(growthcurverOutputPlot$plate))
-
-growthcurverOutputPlot$label = growthcurverOutputPlot$strain
-growthcurverOutputPlot$label = paste(growthcurverOutputPlot$label, growthcurverOutputPlot$plate, sep="")
-
-growthcurverOutputPlot$type = growthcurverOutputPlot$strain
-growthcurverOutputPlot$type[growthcurverOutputPlot$type == 2490 | growthcurverOutputPlot$type ==  2492] = "Deletion"
-growthcurverOutputPlot$type[growthcurverOutputPlot$type == 2491 | growthcurverOutputPlot$type ==  2493] = "Control"
-
-growthcurverOutputPlot$sorter = growthcurverOutputPlot$media
-growthcurverOutputPlot$sorter = paste(growthcurverOutputPlot$sorter, growthcurverOutputPlot$type, sep = "-")
-
-grep("Mm", growthcurverOutputPlot$sample)
-growthcurverOutputPlotTrimmed = growthcurverOutputPlot[grep("Mm", growthcurverOutputPlot$sample),]
-
-growthcurverOutputPlotTrimmed = growthcurverOutputPlotTrimmed[-grep("Ser_5", growthcurverOutputPlotTrimmed$sample),]
-growthcurverOutputPlotTrimmed = growthcurverOutputPlotTrimmed[-grep("Thr_5", growthcurverOutputPlotTrimmed$sample),]
-growthcurverOutputPlotTrimmed = growthcurverOutputPlotTrimmed[-grep("Glu_5", growthcurverOutputPlotTrimmed$sample),]
-growthcurverOutputPlotTrimmed = growthcurverOutputPlotTrimmed[-grep("As_1", growthcurverOutputPlotTrimmed$sample),]
-
-
-growthcurverOutputPlotSerine = growthcurverOutputPlotTrimmed
-growthcurverOutputPlotSerine = growthcurverOutputPlotSerine[-grep("As_5", growthcurverOutputPlotSerine$sample),]
-growthcurverOutputPlotSerine = growthcurverOutputPlotSerine[-grep("Thr_1", growthcurverOutputPlotSerine$sample),]
-
-growthcurverOutputPlotThreonine = growthcurverOutputPlotTrimmed
-growthcurverOutputPlotThreonine = growthcurverOutputPlotThreonine[-grep("As_5", growthcurverOutputPlotThreonine$sample),]
-growthcurverOutputPlotThreonine = growthcurverOutputPlotThreonine[-grep("Ser_1", growthcurverOutputPlotThreonine$sample),]
-
-
-
-ggplot(growthcurverOutputPlotTrimmed, aes(x= strain, y= r, colour=strain, label=concentration))+
-  geom_point()+
-  geom_text(hjust=0, vjust=0)
-# 
-ggplot(growthcurverOutputPlotTrimmed, aes(x= strain, y= k, colour=media, label=concentration))+
-  geom_point()+
-  geom_text(hjust=0, vjust=0)
-
-#
 
 
 mediaK = ggplot(growthcurverOutputPlotTrimmed, aes(x= media, y= k, colour=label, label=concentration))+
   scale_color_manual(values = colorset)+
   ylim(0, 1)+
-  ylab("Carrying Capacity")+
+  ylab("Carrying Capacity [K]")+
   geom_jitter(width = 0.1, size = 3)
 mediaK
 
 
 mediaR = ggplot(growthcurverOutputPlotTrimmed, aes(x= media, y= r, colour=label))+
   scale_color_manual(values = colorset)+
-  ylab("Growth Rate")+
+  ylab("Growth Rate [r]")+
   geom_jitter(width = 0.1, size = 3)
 mediaR
 
@@ -231,34 +226,38 @@ grid.arrange(mediaK, mediaR, ncol = 2)
 # - 
 
 backgroundBlue = c("cornflowerblue", "cornflowerblue", "cornflowerblue")
-backgroundRed = c("indianred2", "indianred2", "indianred2")
+backgroundRed = c("lightcoral", "lightcoral", "lightcoral")
 colorset = c(backgroundRed, backgroundBlue,  backgroundRed, backgroundBlue)
 
 serineK = ggplot(growthcurverOutputPlotSerine, aes(x= sorter, y= k, colour=label, label=concentration))+
   scale_color_manual(values = colorset)+
   ylim(0, 1)+
-  ylab("Carrying Capacity")+
-  geom_jitter(width = 0.1, size = 3)
+  ylab("Carrying Capacity [K]")+
+  geom_jitter(width = 0.1, size = 3)+
+  theme_bw()
 serineK
 
 
 threonineK = ggplot(growthcurverOutputPlotThreonine, aes(x= sorter, y= k, colour=label, label=concentration))+
   scale_color_manual(values = colorset)+
   ylim(0, 1)+
-  ylab("Carrying Capacity")+
-  geom_jitter(width = 0.1, size = 3)
+  ylab("Growth Rate [r]")+
+  geom_jitter(width = 0.1, size = 3)+
+  theme_bw()
 threonineK
 
 
 serR = ggplot(growthcurverOutputPlotSerine, aes(x= sorter, y= r, colour=label))+
   scale_color_manual(values = colorset)+
   ylab("Growth Rate")+
-  geom_jitter(width = 0.1, size = 3)
+  geom_jitter(width = 0.1, size = 3)+
+  theme_bw()
 serR
 
 
 thrR = ggplot(growthcurverOutputPlotThreonine, aes(x= sorter, y= r, colour=label))+
   scale_color_manual(values = colorset)+
   ylab("Growth Rate")+
-  geom_jitter(width = 0.1, size = 3)
+  geom_jitter(width = 0.1, size = 3)+
+  theme_bw()
 thrR
